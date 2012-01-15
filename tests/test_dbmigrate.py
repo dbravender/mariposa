5715a14081e24ad678ec0911e7266269b4419778
Dan Bravender <dan.bravender@gmail.com>
2012-01-16 06:59:50 +0900

    PEP-8 line width

--- tests/test_dbmigrate.py
+++ tests/test_dbmigrate.py
@@ -46,16 +46,19 @@ class TestDBMigrate(object):
         self.settings['directory'] = fixtures_path
         self.settings['dry_run'] = True
         dbmigrate = DBMigrate(**self.settings)
-        assert dbmigrate.migrate() == """BEGIN;
--- start filename: 20120115075349-create-user-table.sql sha1: 00fe6624203fd0be1a6d359bf01341f18d325834
+        assert dbmigrate.migrate() == ("""BEGIN;
+-- start filename: 20120115075349-create-user-table.sql sha1: """
+"""00fe6624203fd0be1a6d359bf01341f18d325834
 -- intentionally making this imperfect so it can be migrated
 CREATE TABLE users (
   id int AUTO_INCREMENT PRIMARY KEY,
   name varchar(255),
   password_sha1 varchar(40)
 );
-INSERT INTO dbmigration (filename, sha1, date) VALUES ('20120115075349-create-user-table.sql', '00fe6624203fd0be1a6d359bf01341f18d325834', datetime());
-COMMIT;"""
+INSERT INTO dbmigration (filename, sha1, date) VALUES ("""
+"""'20120115075349-create-user-table.sql', """
+"""'00fe6624203fd0be1a6d359bf01341f18d325834', datetime());
+COMMIT;""")
 
     def test_initial_migration(self):
         fixtures_path = os.path.join(
